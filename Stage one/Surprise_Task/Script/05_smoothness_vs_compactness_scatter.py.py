2dd92062b53204b769fc44543ecbcf05753153d7
Opeoluwa-Shodipe <119325728+Opeoluwa-Shodipe@users.noreply.github.com>
2025-11-11 22:19:07 +0100

    Update 05_smoothness_vs_compactness_scatter.py.py

--- Stage one/Surprise_Task/Script/05_smoothness_vs_compactness_scatter.py.py
+++ Stage one/Surprise_Task/Script/05_smoothness_vs_compactness_scatter.py.py
@@ -1,3 +1,13 @@
+"""
+Hackbio Internship - Step 1 Surprise Task
+Team: Glycine
+
+# Task: Reproduce Analysis
+# Author: Opeoluwa Shodipe
+# Github: https://github.com/Opeoluwa-Shodipe/Single-Cell-RNA-Sequencing/tree/main/Stage%20one
+# Linkedin: https://www.linkedin.com/in/sopeoluwa/
+"""
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
